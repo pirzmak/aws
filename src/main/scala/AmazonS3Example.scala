064e93b1cd8c6069bec0b54304a097d84ce10764
szyborodziuk <szyborodziuk@gmail.com>
2019-10-24 21:15:02 +0200

    3

--- src/main/scala/AmazonS3Example.scala
+++ src/main/scala/AmazonS3Example.scala
@@ -1,20 +1,20 @@
-import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.{AWSCredentialsProvider, AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.regions.{Region, RegionImpl, Regions}
 import com.amazonaws.services.s3.{AmazonS3Client, AmazonS3ClientBuilder}
 
 object AmazonS3Example {
   def main(args: Array[String]): Unit = {
-    val accessKey = "AKIAJNSEFYYHD43JLEKQ"
-    val secretKey = "8c0NOloloTrololoOloloTrololoOloloTrololo"
-    val bucketName = "demo-bucket"
-    val urlPrefix = "http://localhost:4572"
+    val accessKey = "tmp"
+    val secretKey = "tmp"
+    val bucketName = ""
+    val urlPrefix = ""
 
     val credentials = new BasicAWSCredentials(accessKey, secretKey)
     val client = AmazonS3ClientBuilder.standard().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
       urlPrefix,
-      "tmp")).build()
-
-    print(client.listObjects(bucketName).toString)
+      "tmp")).withCredentials(new AWSStaticCredentialsProvider(credentials)).build()
+//    client.listBuckets()
+    print(client.listObjects(bucketName).getObjectSummaries)
   }
 }
