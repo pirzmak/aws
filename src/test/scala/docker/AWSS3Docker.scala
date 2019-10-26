6dedcbb9066ce80a4c2c0b3072b53a3d2f99beb7
szyborodziuk <szyborodziuk@gmail.com>
2019-10-26 16:54:45 +0200

    5

--- src/test/scala/docker/AWSS3Docker.scala
+++ src/test/scala/docker/AWSS3Docker.scala
@@ -6,11 +6,12 @@ import java.nio.file.Paths
 
 import com.spotify.docker.client.messages.{ContainerConfig, HostConfig, RegistryAuth}
 import com.spotify.docker.client.{DefaultDockerClient, DockerClient}
+import scala.jdk.CollectionConverters._
 
 trait AWSS3Docker {
-  private val docker = DefaultDockerClient.builder().uri("unix://var/run/docker.sock").build()
+  private val docker = DefaultDockerClient.fromEnv().build()
   private val imageName = "awss3docker" + System.nanoTime()
-  docker.build(Paths.get(""), imageName)
+  docker.build(Paths.get("test/scala/docker").toAbsolutePath, imageName)
 
   val hostConfig = HostConfig.builder().build
 
@@ -23,13 +24,20 @@ trait AWSS3Docker {
     null
   ))
 
+  val env: java.util.List[String] = List("DOCKER_HOST=unix:///var/run/docker.sock", "SERVICES=s3").asJava
+  val ports: java.util.List[String] = List("4567-4584:4567-4584").asJava
+
   val containerConfig = ContainerConfig.builder()
     .hostConfig(hostConfig)
     .entrypoint()
     .image(imageName)
+    .portSpecs(ports)
+    .env(env)
     .build
 
   val creation = docker.createContainer(containerConfig)
 
   docker.startContainer(creation.id)
+
+  docker.listContainers()
 }
